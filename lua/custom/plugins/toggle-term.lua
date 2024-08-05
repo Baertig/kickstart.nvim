186120f1eb0e39309abc32dfdbe28fa1b8a49c47
Georg Bär-Dumont <georg.baer-dumont@relaxdays.de>
2024-08-05 07:33:25 +0200

    install toggle-term

---
+++ lua/custom/plugins/toggle-term.lua
@@ -0,0 +1,21 @@
+return {
+  'akinsho/toggleterm.nvim',
+  config = function()
+    require('toggleterm').setup {
+      direction = 'vertical',
+      size = 100,
+    }
+
+    local Terminal = require('toggleterm.terminal').Terminal
+    local lazygit = Terminal:new { cmd = 'lazygit', hidden = true, direction = 'float', display_name = 'git' }
+
+    vim.keymap.set('n', '<c-j>', ':ToggleTerm direction=vertical<cr>')
+    vim.keymap.set('n', '<c-s-j>', ':ToggleTerm direction=float<cr>')
+    vim.keymap.set('t', '<c-j>', '<C-\\><C-n>:ToggleTerm<cr>')
+    vim.keymap.set('t', '<c-s-j>', '<C-\\><C-n>:ToggleTerm<cr>')
+
+    vim.keymap.set('n', '<leader>gs', function()
+      lazygit:toggle()
+    end, { nowait = true, silent = true })
+  end,
+}
