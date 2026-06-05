f01164b1388577915d6bc813e7a14b8feb07fd5f
Baer-Dumont <georg.baer-dumont@sap.com>
2026-06-05 09:52:14 +0200

    update typescript language server (package name + config changed)

--- init.lua
+++ init.lua
@@ -510,20 +510,9 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        tsserver = {
-          init_options = {
-            plugins = {
-              {
-                name = '@vue/typescript-plugin',
-                location = require('mason-registry').get_package('vue-language-server'):get_install_path() .. '/node_modules/@vue/language-server',
-                -- location = '/home/georgbaerdumont/.local/share/nvim/mason/packages/vue-language-server/node_modules/@vue/language-server',
-                languages = { 'vue' },
-              },
-            },
-          },
-          filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+        ts_ls = {
+          filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact' },
         },
-        volar = {},
         cssls = {},
         html = {},
         --
